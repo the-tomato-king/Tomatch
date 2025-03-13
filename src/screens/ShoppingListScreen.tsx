e648eecc21182a827a8e1655f7217c10b8e222eb
gnblink0 <gnblink44@gmail.com>
2025-03-12 17:37:34 -0700

    feat: create 4 basic screens

---
+++ src/screens/ShoppingListScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ShoppingListScreen = () => {
+  return (
+    <View>
+      <Text>ShoppingListScreen</Text>
+    </View>
+  )
+}
+
+export default ShoppingListScreen
+
+const styles = StyleSheet.create({})
