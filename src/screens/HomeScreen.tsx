72243b6dc7a2f2f1eb569261dbc6b83f9d6a376a
gnblink0 <gnblink44@gmail.com>
2025-03-12 22:56:15 -0700

    feat: update HomeScreen layout to display title with new styles

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import { styles } from '../theme/styles'
 const HomeScreen = () => {
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>All Products</Text>
+      </View>
     </View>
-  )
+  );
 }
 
 export default HomeScreen
-
-const styles = StyleSheet.create({})
---
+++ src/theme/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+import colors from "./colors";
+
+export const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+    backgroundColor: "pink",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 20,
+  },
+});
