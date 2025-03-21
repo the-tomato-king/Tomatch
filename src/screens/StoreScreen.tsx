3907fbb2448345fcebe92f112ec5b84f3ee499d6
gnblink0 <gnblink44@gmail.com>
2025-03-21 01:29:24 -0700

    feat: create basic layout of StoreScreen

--- src/screens/StoreScreen.tsx
+++ src/screens/StoreScreen.tsx
@@ -1,14 +1,87 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
+import React from "react";
 
 const StoreScreen = () => {
   return (
-    <View>
-      <Text>StoreScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.container}>
+      <View style={styles.scrollView}>
+        {/* Search Section */}
+        <View style={styles.searchSection}>
+          {/* TODO:Search bar will go here */}
+          <Text>Search</Text>
+        </View>
 
-export default StoreScreen
+        {/* Map Section */}
+        <View style={styles.mapSection}>
+          {/* Location Section */}
+          <View style={styles.locationSection}>
+            <Text style={styles.locationText}>
+              Current Location: 7395 maple st
+            </Text>
+          </View>
+          <View style={styles.mapContainer}>
+            <Text>TODO: Implement google map api</Text>
+          </View>
+        </View>
+        <View style={styles.storesListSection}>
+          <View style={styles.tabsSection}>
+            <Text>Favorites</Text>
+            <Text>Nearby</Text>
+          </View>
+          <View style={styles.storesListSection}>
+            <Text>List of stores will go here</Text>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default StoreScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  scrollView: {
+    flex: 1,
+  },
+  headerSection: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#e0e0e0",
+  },
+  headerTitle: {
+    fontSize: 28,
+    fontWeight: "bold",
+  },
+  searchSection: {
+    padding: 16,
+  },
+  locationSection: {
+    paddingHorizontal: 16,
+    paddingBottom: 16,
+  },
+  locationText: {
+    
+    fontSize: 16,
+  },
+  mapSection: {
+    padding: 16,
+    alignItems: "center",
+
+  },
+  mapContainer: {
+    width: "100%",
+    height: 300,
+    backgroundColor: "pink",
+  },
+  tabsSection: {
+    padding: 16,
+  },
+  storesListSection: {
+    padding: 16,
+    backgroundColor: "pink",
+  },
+});
