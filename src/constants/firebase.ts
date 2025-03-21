2fcc2fc6608c63633f0287f8261c6ee10c2184ac
gnblink0 <gnblink44@gmail.com>
2025-03-21 00:56:10 -0700

    refactor: update Firebase constants and user-related types to support brands and user stores

--- src/constants/firebase.ts
+++ src/constants/firebase.ts
@@ -1,12 +1,13 @@
 export const COLLECTIONS = {
   USERS: "users",
   PRODUCTS: "products",
-  STORES: "stores",
+  BRANDS: "brands",
   SUB_COLLECTIONS: {
     CUSTOMIZED_PRODUCTS: "customized_products",
     SHOPPING_LISTS: "shopping_lists",
     PRICE_RECORDS: "price_records",
     USER_PRODUCTS: "user_products",
     USER_PRODUCT_STATS: "user_product_stats",
+    USER_STORES: "user_stores",
   },
 } as const;
